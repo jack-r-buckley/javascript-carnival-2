1a760c8a3614b7d13b9b5ee3b3b060647f065008
Jack Buckley <jackrbuckley@gmail.com>
2021-03-08 17:06:57 +1300

    make game functional

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,15 +1,38 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-let gameBoard = document.getElementById("gameBoard")
-console.log(gameBoard)
+  let gameBoard = document.getElementById("gameBoard")
+  let gameBoardIndex = []
+  let newCell = Math.floor(Math.random() * 25 )
+  let clickedCell = 0
+  // document.addEventListener('click', changeMole())
+  // changeMole()
 
-let gameBoardIndex = []
+  for (let i = 0; i < gameBoard.rows.length; i++) {
+    for (let j = 0; j < gameBoard.rows[i].cells.length; j++) {
+      gameBoardIndex.push(gameBoard.rows[i].cells[j])
+      gameBoard.rows[i].cells[j].setAttribute('onclick', 'readGameBoard(this)')
+      gameBoard.rows[i].cells[j].hasMole = false
+    }
+  }
 
-for (let i = 0; i < gameBoard.rows.length; i++) {
-  for (let j = 0; j < gameBoard.rows[i].cells.length; j++) {
-    gameBoardIndex.push(gameBoard.rows[i].cells[j])
+  for (let i = 0; i < gameBoardIndex.length; i++) {
+    gameBoardIndex[i].indexPosition = i
+  }
+
+  gameBoardIndex[newCell].hasMole = true
+  gameBoardIndex[newCell].innerHTML = '<img src="mole.PNG" width="100%" height="100%" />'
+  
+  function readGameBoard(clickedCell) {
+if (clickedCell.indexPosition == newCell) {
+      var bonkNoise = new Audio('whack-audio.wav');
+      bonkNoise.play();
+      gameBoardIndex[newCell].innerHTML = ''
+      gameBoardIndex.hasMole = false
+      newCell = Math.floor(Math.random() * 25)
+      gameBoardIndex[newCell].innerHTML = '<img src="mole.PNG" width="100%" height="100%" />'
+      gameBoardIndex.hasMole = true
   }
 }
-  
-console.log(gameBoardIndex)
+
+
