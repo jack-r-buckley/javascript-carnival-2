bd6e68dce466cb61f0923f9855a692798a7bffba
Jack Buckley <jackrbuckley@gmail.com>
2021-03-08 13:41:58 +1300

    pull gameboard table into js variable and create an index

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,15 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+let gameBoard = document.getElementById("gameBoard")
+console.log(gameBoard)
 
-console.log("Whack-a-Mole!")
+let gameBoardIndex = []
+
+for (let i = 0; i < gameBoard.rows.length; i++) {
+  for (let j = 0; j < gameBoard.rows[i].cells.length; j++) {
+    gameBoardIndex.push(gameBoard.rows[i].cells[j])
+  }
+}
+  
+console.log(gameBoardIndex)
